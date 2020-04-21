6c0569dfc644f0012ac637c294f4447c009d55c1
wongro2 <roy.wong@oocl.com>
2020-04-21 15:19:20 +0800

    [Roy] created counter and plus/minus buttons

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter></Counter>
+        </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,39 @@
+import React, { Component} from "react";
+
+
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+
+		this.onIncrease = this.onIncrease.bind(this);
+		this.onDecrease = this.onDecrease.bind(this);
+
+        this.state = {
+            number: 0,
+        };
+    }
+
+    
+onIncrease(){
+    this.setState((prevState) => ({number: prevState.number+1}));
+}
+onDecrease(){
+    this.setState((prevState) => ({number: prevState.number-1}));
+}
+    
+render(){
+	return(
+			<div>
+                <h2  >Counter</h2>
+                <button onClick={this.onIncrease}>+ </button>
+                <span>{this.state.number}</span>
+                <button onClick={this.onDecrease}> -</button>
+			
+			</div>
+			);
+	}
+}
+
+
+export default Counter;
